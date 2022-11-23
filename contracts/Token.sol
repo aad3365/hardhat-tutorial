cfe33fabdc768d80a133f278333335cce85613cc
aad3365 <aad336545@gmail.com>
2022-11-23 22:30:19 +0900

    add console.log, deploy script, connect goerli network

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,6 +4,7 @@
 // It will be used by the Solidity compiler to validate its version.
 pragma solidity ^0.8.9;
 
+import "hardhat/console.sol";
 
 // This is the main building block for smart contracts.
 contract Token {
@@ -46,6 +47,12 @@ contract Token {
         // transaction will revert.
         require(balances[msg.sender] >= amount, "Not enough tokens");
 
+        console.log(
+            "Transferring from %s to %s %s tokens",
+            msg.sender,
+            to,
+            amount
+        );
         // Transfer the amount.
         balances[msg.sender] -= amount;
         balances[to] += amount;
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
 require('@nomicfoundation/hardhat-toolbox');
+require('dotenv').config();
+
+const alchemyApiKey = process.env.ALCHEMY_API_KEY;
+const goerliPrivateKey = process.env.GOERLI_PRIVATE_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.g.alchemy.com/v2/${alchemyApiKey}`,
+      accounts: [goerliPrivateKey]
+    }
+  }
 };
